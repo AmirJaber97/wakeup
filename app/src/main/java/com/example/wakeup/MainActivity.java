c4aabc06c1f8a7c2295928dba3b5f8fcd5f4d707
Amir Jaber <amir.jaber@rogmax.com>
2019-08-23 16:21:43 +0300

    WIP

--- app/src/main/java/com/example/wakeup/MainActivity.java
+++ app/src/main/java/com/example/wakeup/MainActivity.java
@@ -7,12 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
     private Sensor proximitySensor;
     private SensorEventListener proximitySensorListener;
@@ -39,8 +41,19 @@ public class MainActivity extends AppCompatActivity {
             public void onSensorChanged(SensorEvent sensorEvent) {
                 if (sensorEvent.values[0] < proximitySensor.getMaximumRange()) {
                     // WAKE UP
-                    Toast.makeText(getApplicationContext(), "Waking Screen Up", Toast.LENGTH_LONG).show();
-                    wakeupScreen();
+                    // CHECK IF SCREEN IS ON OR OFF TO NOT CALL THE FUNCTION ALL THE TIME
+                    // MAKE WAKEUP A SERVICE https://examples.javacodegeeks.com/android/core/service/android-service-example/
+                    // MAKE IT LOAD IN A SMALL INTERFACE, ALLOW ADDING THE WIDGET & ENABLE/DISABLE SERVICE
+                    PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT_WATCH) {
+                        boolean isScreenOn = powerManager.isInteractive();
+                        if (!isScreenOn) {
+                            Toast.makeText(getApplicationContext(), "Waking Screen Up", Toast.LENGTH_LONG).show();
+                            wakeupScreen();
+                        } else {
+                            Log.d(TAG, "onSensorChanged: Screen is Already On");
+                        }
+                    }
                 }
             }
 
@@ -57,15 +70,11 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("InvalidWakeLockTag")
     private void wakeupScreen() {
-        PowerManager.WakeLock screenLock = null;
-        if ((getSystemService(POWER_SERVICE)) != null) {
-            screenLock = ((PowerManager) getSystemService(POWER_SERVICE)).newWakeLock(
-                    PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
-            screenLock.acquire(10 * 60 * 1000L /*10 minutes*/);
+        PowerManager.WakeLock screenLock = ((PowerManager) getSystemService(POWER_SERVICE)).newWakeLock(
+                PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        screenLock.acquire();
 
-
-            screenLock.release();
-        }
+        screenLock.release();
     }
 
 
